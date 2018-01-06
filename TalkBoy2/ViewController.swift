faa5ad430e271d4c9c3b5f1aa2c43ca3855b8dea
Michael Wortman <mwortman@outlook.com>
2018-01-06 15:55:48 -0800

    Initial Commit

---
+++ TalkBoy2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TalkBoy2
+//
+//  Created by Michael on 2018-01-06.
+//  Copyright © 2018 Differential Consulting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
