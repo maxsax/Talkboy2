2555d7b75e5ce368ea55dd6fe952c558c86024c5
Unknown <mwortman@outlook.com>
2018-01-06 20:56:21 -0800

    Version 1.0 completed

--- TalkBoy2/AddClipViewController.swift
+++ TalkBoy2/AddClipViewController.swift
@@ -7,29 +7,102 @@
 //
 
 import UIKit
+import AVFoundation
 
 class AddClipViewController: UIViewController {
 
+    
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var addButton: UIButton!
+    
+    
+    
+    var audioRecorder: AVAudioRecorder?
+    var audiopPlayer: AVAudioPlayer?
+    var audioURL: URL?
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        // Setup the audio recorder
+        let session = AVAudioSession.sharedInstance()
+        try? session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        try? session.overrideOutputAudioPort(.speaker)
+        try? session.setActive(true)
+        
+        // URL to save audion to sandbox
+        if let basePath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first {
+            let pathComponents = [basePath, "audio.m4a"] // path compnenetrs needs to be an array
+            
+            if let audioURL = NSURL.fileURL(withPathComponents: pathComponents) {
+                // Create settings
+                
+                self.audioURL = audioURL
+                var settings : [String:Any] = [:]
+                settings[AVFormatIDKey] = Int(kAudioFormatMPEG4AAC)
+                settings[AVSampleRateKey] = 44100.0
+                settings[AVNumberOfChannelsKey] = 2
+                
+                // Create audio recorder
+                audioRecorder = try? AVAudioRecorder(url: audioURL, settings: settings)
+                audioRecorder?.prepareToRecord()
+            }
+        }
+        
+        // Setup UI state
+        recordButton.isEnabled = true
+        playButton.isEnabled = false
+        addButton.isEnabled = false
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    
+    @IBAction func recordTapped(_ sender: Any) {
+        if let audioRecorder = self.audioRecorder {
+            
+            if audioRecorder.isRecording {
+                audioRecorder.stop()
+                recordButton.setTitle("Record", for: .normal)
+                recordButton.isEnabled = true
+                playButton.isEnabled = true
+                addButton.isEnabled = true
+                
+            } else {
+                audioRecorder.record()
+                recordButton.setTitle("Stop", for: .normal)
+                recordButton.isEnabled = true
+                playButton.isEnabled = false
+                addButton.isEnabled = false
+            }
+        }
     }
-    */
-
+    
+    
+    
+    @IBAction func playTapped(_ sender: Any) {
+        if let audioURL = self.audioURL {
+            try? audiopPlayer = AVAudioPlayer(contentsOf: audioURL)
+            audiopPlayer?.play()
+        }
+    }
+    
+    
+    
+    @IBAction func addTapped(_ sender: Any) {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            let clip = Clip(entity: Clip.entity(), insertInto: context)
+            clip.name = nameTextField.text
+            
+            if let audioURL = self.audioURL {
+                clip.audioData = try? Data(contentsOf: audioURL)
+                try? context.save()
+            }
+            
+            navigationController?.popViewController(animated: true)
+        }
+    }
 }
--- TalkBoy2/ClipTableViewController.swift
+++ TalkBoy2/ClipTableViewController.swift
@@ -7,89 +7,90 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ClipTableViewController: UITableViewController {
 
+    
+    var clips: [Clip] = []
+    var audioPlayer: AVAudioPlayer? = nil
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        getClips()
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    
+    
+    func getClips() {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            if let coreDataStuff = try? context.fetch(Clip.fetchRequest()) as? [Clip] {
+                if let coreDataClips = coreDataStuff {
+                    clips = coreDataClips
+                    tableView.reloadData()
+                }
+            }
+        }
     }
-
+    
+    
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return clips.count
     }
 
-    /*
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let clip = clips[indexPath.row]
 
-        // Configure the cell...
-
+        cell.textLabel?.text = clip.name
+        
         return cell
     }
-    */
+    
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
-
-    /*
+ 
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let clip = clips[indexPath.row]
+        
+        if let audioData = clip.audioData {
+            audioPlayer = try? AVAudioPlayer(data: audioData)
+            audioPlayer?.play()
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            let clip = clips[indexPath.row]
+            if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+                context.delete(clip)
+                try? context.save()
+                getClips()
+            }
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
